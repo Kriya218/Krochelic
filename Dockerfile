14c073d386f4aa6855c2e05c4f826c8355e08022
Kriya <kriya101101@gmail.com>
2024-12-03 17:32:01 +0800

    Add nginx

--- Dockerfile
+++ Dockerfile
@@ -9,15 +9,10 @@ RUN apk add --no-cache \
   && rm -rf /var/cache/apk/*
 COPY package*.json ./
 RUN npm install
-
-COPY config/config.json /app/config/config.json
 COPY . .
 
-COPY entrypoint.sh /usr/entrypoint.sh
-COPY wait-for-it.sh /usr/wait-for-it.sh
+RUN dos2unix /app/entrypoint.sh /app/wait-for-it.sh
+RUN chmod +x /app/entrypoint.sh /app/wait-for-it.sh
 
-RUN dos2unix /usr/entrypoint.sh /usr/wait-for-it.sh
-RUN chmod +x /usr/entrypoint.sh /usr/wait-for-it.sh
-RUN ls -l /app
 EXPOSE 3000
-ENTRYPOINT ["/usr/entrypoint.sh"]
+ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,17 @@ echo "Starting entrypoint.sh..."
 # 使用 wait-for-it.sh 腳本檢查 MySQL 服務是否已啟動並可用
 /usr/wait-for-it.sh db:3306 --timeout=30 --strict -- echo "MySQL is ready - starting app"
 
-# 執行 Sequelize 資料庫遷移
+
 echo "Running database migrations..."
 npx sequelize db:migrate
-npx sequelize db:seed:all
+
+# 判斷種子檔案是否已執行過
+if [ $(npx sequelize-cli db:seed:status | grep -c "No seeds were executed.") -gt 0 ]; then
+    echo "Running database seeding..."
+    npx sequelize db:seed:all
+else
+    echo "Database already seeded - skipping seeding."
+fi
+
 echo "Starting Express app"
-# export NODE_ENV=development
 npm start
